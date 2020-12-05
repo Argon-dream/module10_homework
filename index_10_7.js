81ea2f69b5dc4b8a0f146c15f15e18b91fffb068
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-12-05 17:26:50 +0100

    Проверено

--- index_10_7.js
+++ index_10_7.js
@@ -1,29 +1,24 @@
 let arr = [4,'r',5,7,23,6,null,NaN,0];
-let oddElem = 0;
-let evenElem = 0;
-let zeroElem = 0;
-let otherElem = 0;
+// Здесь можно чуть сократить код и объявить переменные в одну строку
+let oddElem = 0, evenElem = 0, zeroElem = 0, otherElem = 0;
 for (let i=0; i<arr.length;i++)
 {
-    if ((typeof arr[i] != "number") || (isNaN(arr[i])))
+    if (typeof arr[i] != "number" || isNaN(arr[i])) // скобки в данном случае лишние
     {
         otherElem=otherElem+1; //суммируем не числовые элементы
     }
+    else if (arr[i] == 0)
+    {
+        zeroElem=zeroElem+1;
+    } // суммируем нули
+    else if (arr[i] % 2 == 0)
+    {
+        evenElem=evenElem+1;
+    } // суммируем чётные числа
     else
     {
-        if (arr[i] == 0)
-        {
-            zeroElem=zeroElem+1;
-        } // суммируем нули
-        else if (arr[i] % 2 == 0)
-        {
-            evenElem=evenElem+1;
-        } // суммируем чётные числа
-        else
-            {
-            oddElem=oddElem+1;
-            } // суммируем нечётные числа
-    }
+    oddElem=oddElem+1;
+    } // суммируем нечётные числа
 }
 console.log("Number of even: ", evenElem);
 console.log("Number of odd: ", oddElem);
